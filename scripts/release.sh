21097dfff2ead678f0b30a8e361863c417939f97
Andrew Lisowski <lisowski54@gmail.com>
2018-12-07 16:28:52 -0800

    add npm to path

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,6 @@ git config --global user.email "lisowski54@gmail.com"
 git config --global user.name "Andrew Lisowski"
 
 chmod +x ./dist/bin/auto.js
+export PATH=$(npm bin):$PATH
 
 ./dist/bin/auto.js shipit
