32085168eff970a06c1d1d3e3e97eb113f4c0b23
Andrew Lisowski <lisowski54@gmail.com>
2018-12-15 03:14:47 -0800

    delete old unneeded test

--- src/cli/__tests__/args.test.ts
+++ src/cli/__tests__/args.test.ts
@@ -37,32 +37,3 @@ describe('root parser', () => {
     });
   });
 });
-
-// describe('label', () => {
-//   let labelParser: ArgumentParser;
-
-//   beforeEach(() => {
-//     labelParser = new ArgumentParser({
-//       addHelp: true,
-//       description: 'label'
-//     });
-
-//     addLabelParser(labelParser);
-//   });
-
-//   test('gets the pr number', () => {
-//     expect(labelParser.parseArgs('--pr 100'.split(' ')).pr).toBe('100');
-//   });
-//   test('gets all the things', () => {
-//     expect(
-//       labelParser.parseArgs('--repo test-repo --pr 10 --owner adam'.split(' '))
-//     ).toEqual({
-//       repo: 'test-repo',
-//       pr: '10',
-//       owner: 'adam',
-//       verbose: false,
-//       very_verbose: false,
-//       githubApi: null
-//     });
-//   });
-// });
