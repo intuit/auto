e79b1793bf3112a008a9ac88c2407ea96e3c00e2
Andrew Lisowski <lisowski54@gmail.com>
2019-06-13 16:41:29 -0700

    get previous version from last git tag

--- plugins/git-tag/src/index.ts
+++ plugins/git-tag/src/index.ts
@@ -5,6 +5,16 @@ export default class GitTagPlugin implements IPlugin {
   name = 'Git Tag';
 
   apply(auto: Auto) {
+    auto.hooks.getPreviousVersion.tapPromise(this.name, async () => {
+      if (!auto.git) {
+        throw new Error(
+          "Can't calculate previous version without Git initialized!"
+        );
+      }
+
+      return auto.git.getLatestTagInBranch();
+    });
+
     auto.hooks.version.tapPromise(this.name, async version => {
       if (!auto.git) {
         return;
