b0e5220ec3608429a84dcd9eb5cd2f1985619b97
Andrew Lisowski <lisowski54@gmail.com>
2019-11-10 23:18:59 -0800

    set git user

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -6,6 +6,8 @@ if [ ! -z "$SHOULD_PUBLISH" ]; then
   echo publishing $SHOULD_PUBLISH
   ./docs/generate-command-docs.js
   ignite
+  git config --global user.name "Andrew Lisowski"
+  git config --global user.email "lisowski54@gmail.com"
   npx push-dir --cleanup --dir=_ignite/auto --branch=gh-pages
 else
   echo No documentation files changed since last tagged release.
