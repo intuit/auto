14a691c89b4730b3e068bba19284c4e8d4ad484b
Andrew Lisowski <lisowski54@gmail.com>
2020-01-24 14:51:47 -0800

    add test

---
+++ packages/core/src/utils/__tests__/test-plugin-malformed.js
@@ -0,0 +1,12 @@
+module.expss Test {
+  /** The name of the plugin */
+  name = 'foo';
+
+  /** The options of the plugin */
+  config: {};
+
+  /** Initialize the plugin with it's options */
+  constructor(config: {}) {
+    this.config = config;
+  }
+};
