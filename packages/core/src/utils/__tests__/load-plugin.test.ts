fdd16587f12cf7c63996cb05cb81aa3212d5d4da
Andrew Lisowski <lisowski54@gmail.com>
2020-02-24 17:43:28 -0800

    add scoped plugin support

--- packages/core/src/utils/__tests__/load-plugin.test.ts
+++ packages/core/src/utils/__tests__/load-plugin.test.ts
@@ -4,8 +4,56 @@ import { dummyLog } from '../logger';
 
 const logger = dummyLog();
 
+jest.mock(
+  'auto-plugin-foo',
+  () => ({
+    default: class {
+      name = 'foo';
+    }
+  }),
+  {
+    virtual: true
+  }
+);
+jest.mock(
+  '@my-scope/auto-plugin-bar',
+  () => ({
+    default: class {
+      name = 'bar';
+    }
+  }),
+  { virtual: true }
+);
+jest.mock(
+  '@auto-it/baz',
+  () => ({
+    default: class {
+      name = 'baz';
+    }
+  }),
+  {
+    virtual: true
+  }
+);
+
 describe('loadPlugins', () => {
-  test('should require custom plugins -- fallback to cwd', async () => {
+  test('should load official plugins', () => {
+    expect(loadPlugin(['baz', {}], logger)?.name).toBe('baz');
+    expect(loadPlugin(['@auto-it/baz', {}], logger)?.name).toBe('baz');
+  });
+
+  test('should load community plugins', () => {
+    expect(loadPlugin(['foo', {}], logger)?.name).toBe('foo');
+    expect(loadPlugin(['auto-plugin-foo', {}], logger)?.name).toBe('foo');
+  });
+
+  test('should load scoped plugins', () => {
+    expect(
+      loadPlugin(['@my-scope/auto-plugin-bar', {}], logger)?.name
+    ).toBe('bar');
+  });
+
+  test('should require custom plugins -- fallback to cwd', () => {
     expect(
       loadPlugin([path.join(__dirname, './test-plugin.ts'), {}], logger)
     ).toStrictEqual(
@@ -16,7 +64,7 @@ describe('loadPlugins', () => {
     );
   });
 
-  test('should require custom plugins -- surface errors', async () => {
+  test('should require custom plugins -- surface errors', () => {
     expect(() =>
       loadPlugin(
         [path.join(__dirname, './test-plugin-malformed.js'), {}],
@@ -25,7 +73,7 @@ describe('loadPlugins', () => {
     ).toThrow();
   });
 
-  test('should load config', async () => {
+  test('should load config', () => {
     expect(
       loadPlugin(
         [path.join(__dirname, './test-plugin.ts'), 'do the thing'],
