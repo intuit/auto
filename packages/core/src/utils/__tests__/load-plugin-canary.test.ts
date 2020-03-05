459e600c3c01bbde5f0959d638525fc3f634bcf5
Andrew Lisowski <lisowski54@gmail.com>
2020-03-05 09:00:44 -0800

    Fix loading canary versions of offical plugins

---
+++ packages/core/src/utils/__tests__/load-plugin-canary.test.ts
@@ -0,0 +1,22 @@
+import loadPlugin from '../load-plugins';
+import { dummyLog } from '../logger';
+
+const logger = dummyLog();
+
+jest.mock(
+  '@auto-canary/baz',
+  () => ({
+    default: class {
+      name = 'baz';
+    }
+  }),
+  {
+    virtual: true
+  }
+);
+
+describe('loadPlugins', () => {
+  test('should load canary plugins', () => {
+    expect(loadPlugin(['baz', {}], logger)?.name).toBe('baz');
+  });
+});
