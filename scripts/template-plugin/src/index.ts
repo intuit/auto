b4bb2d6c89c34b9334a3d67410d83e2cf75ba142
Andrew Lisowski <lisowski54@gmail.com>
2020-03-06 10:03:51 -0800

    fix plugin template create

--- scripts/template-plugin/src/index.ts
+++ scripts/template-plugin/src/index.ts
@@ -2,16 +2,17 @@ import { Auto, IPlugin } from '@auto-it/core';
 
 interface I{{pascal}}PluginOptions {}
 
+/** {{description}} */
 export default class {{pascal}}Plugin implements IPlugin {
   /** The name of the plugin */
-  name = '{{title}}';
+  name = '{{kebab}}';
 
   /** The options of the plugin */
   readonly options: I{{pascal}}PluginOptions;
 
   /** Initialize the plugin with it's options */
   constructor(options: I{{pascal}}PluginOptions) {
-    this.options = {};
+    this.options = options;
   }
 
   /** Tap into auto plugin points. */
