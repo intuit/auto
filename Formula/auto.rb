1d440a1ddb0d1fa2d4c48df15ba4786b36834d34
Andrew Lisowski <lisowski54@gmail.com>
2020-05-15 18:13:47 -0700

    Bump auto brew formula [skip ci]

--- Formula/auto.rb
+++ Formula/auto.rb
@@ -1,9 +1,9 @@
 class Auto < Formula
   desc "Generate releases based on semantic version labels on pull requests."
   homepage "https://intuit.github.io/auto/home.html"
-  url "https://github.com/intuit/auto/releases/download/v9.32.2/auto-macos.gz"
-  version "v9.32.2"
-  sha256 "5084e4ad93d9eaa48574a5cc95a3fcb00cde95cb7f6c6ec28c3c180442cf500d"
+  url "https://github.com/intuit/auto/releases/download/v9.32.3/auto-macos.gz"
+  version "v9.32.3"
+  sha256 "8714556672f9a1a5c44bd26e36d630b2253fae515e784cb8ee4aa2863e7092d6"
 
   def install
     libexec.install Dir["*"]
