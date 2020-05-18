debac80197de63d9a4c1348978f793cc36367b09
Andrew Lisowski <lisowski54@gmail.com>
2020-05-18 12:10:14 -0700

    make test clean up and not delete the zip files

--- packages/cli/__tests__/bundle.test.ts
+++ packages/cli/__tests__/bundle.test.ts
@@ -2,18 +2,22 @@ import path from "path";
 import { execSync } from "child_process";
 
 test("bundle should function", () => {
-  const binary =
+  const type =
     (process.platform === "win32" && "win.exe") ||
     (process.platform === "darwin" && "macos") ||
     "linux";
+  const zip = path.join(__dirname, `../binary/auto-${type}`);
+  const binary = path.join(__dirname, "../auto");
 
-  // Using this command because it is unlikely to change very much
+  execSync(`gunzip -c ${zip} > ${binary}`);
+  execSync(`chmod +x ${binary}`);
+
   expect(
-    execSync(
-      path.join(__dirname, `../binary/auto-${binary} create-labels --help`),
-      {
-        encoding: "utf8",
-      }
-    )
+    // Using this command because it is unlikely to change very much
+    execSync(`${binary} create-labels --help`, {
+      encoding: "utf8",
+    })
   ).toMatchSnapshot();
+
+  execSync(`rm ${binary}`);
 });
