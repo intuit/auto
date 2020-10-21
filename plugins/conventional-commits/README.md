fc9cd7d7dbc7bc7405c06a009994fc4e9c5b6d47
Andrew Lisowski <lisowski54@gmail.com>
2020-10-20 18:00:52 -0700

    match conventional commit spec more and allow for conventional changelog presets
    match conventional commit spec more and allow for conventional changelog presets

--- plugins/conventional-commits/README.md
+++ plugins/conventional-commits/README.md
@@ -2,6 +2,15 @@
 
 Parse [conventional commit messages](https://www.conventionalcommits.org/en/v1.0.0-beta.4/) and use them to calculate the version. This plugin will omit the PR HEAD if it isn't labeled and has a commit with a conventional-commit commit message.
 
+The default behavior extends the [conventional commits spec](https://www.conventionalcommits.org/en/v1.0.0/#summary):
+
+- Type `fix:` => `patch`
+- Type `feat:` => `minor`
+- Type `BREAKING:` => `minor`
+- A `!` in the type indicated a breaking change
+- `BREAKING CHANGE` in the footer indicates a breaking change
+- All other types are considered `skip-release`
+
 ## Installation
 
 This plugin is not included with the `auto` CLI installed via NPM. To install:
@@ -19,6 +28,21 @@ yarn add -D @auto-it/conventional-commits
   "plugins": [
     "npm",
     "conventional-commits"
+    // other plugins
+  ]
+}
+```
+
+## Options
+
+You can use any [conventional-changelog preset](https://www.npmjs.com/search?q=conventional-changelog%20preset%20) with this plugin.
+Using a preset will completely override this plugin's default behavior with whatever the preset defines.
+
+```json
+{
+  "plugins": [
+    "npm",
+    ["conventional-commits", { "preset": "angular" }]
     // other plugins
   ]
 }
--- plugins/conventional-commits/src/index.ts
+++ plugins/conventional-commits/src/index.ts
@@ -1,6 +1,79 @@
-import { applyPlugins, mappers, parse } from "parse-commit-message";
+import { Options as CoreOptions } from "conventional-changelog-core";
+import { Commit, sync as parse } from "conventional-commits-parser";
+import { promisify } from "util";
+import conventionalChangelogPresetLoader from "conventional-changelog-preset-loader";
+import * as t from "io-ts";
 
-import { Auto, IPlugin, VersionLabel, SEMVER } from "@auto-it/core";
+import { Auto, IPlugin, SEMVER } from "@auto-it/core";
+// import conventionalCommitsParser, { Commit } from "conventional-commits-parser";
+
+/** Resolve a conventional commit preset */
+function presetResolver(presetPackage: CoreOptions.Config) {
+  if (typeof presetPackage === "function") {
+    return promisify(presetPackage)();
+  }
+
+  // handle object literal or Promise instance
+  if (typeof presetPackage === "object") {
+    return presetPackage;
+  }
+
+  throw new Error("preset package must be a promise, function, or object");
+}
+
+const defaultPreset = {
+  recommendedBumpOpts: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/,
+      breakingHeaderPattern: /^(\w*)(?:\((.*)\))?!: (.*)$/,
+      headerCorrespondence: ["type", "scope", "subject"],
+      noteKeywords: ["BREAKING CHANGE"],
+    },
+    /** Find the bump */
+    whatBump: (commits: Commit[]) => {
+      let level: number | undefined;
+      let breakings = 0;
+      let features = 0;
+
+      commits.forEach((commit) => {
+        if (
+          commit.notes.length > 0 ||
+          commit.type === "BREAKING" ||
+          commit.header?.match(
+            defaultPreset.recommendedBumpOpts.parserOpts.breakingHeaderPattern
+          )
+        ) {
+          breakings += commit.notes.length;
+          level = 0;
+        } else if (commit.type === "feat") {
+          features += 1;
+          if (level === undefined) {
+            level = 1;
+          }
+        } else if (commit.type === "fix" && level === undefined) {
+          level = 2;
+        } else if (commit.type && level === undefined) {
+          level = 3;
+        }
+      });
+
+      return {
+        level: level,
+        reason:
+          breakings === 1
+            ? `There is ${breakings} BREAKING CHANGE and ${features} features`
+            : `There are ${breakings} BREAKING CHANGES and ${features} features`,
+      };
+    },
+  },
+};
+
+const optionalOptions = t.partial({
+  /** A path to the formula template */
+  preset: t.string,
+});
+
+export type ConventionalCommitsOptions = t.TypeOf<typeof optionalOptions>;
 
 /**
  * Parse conventional commit messages and use them to
@@ -10,23 +83,72 @@ export default class ConventionalCommitsPlugin implements IPlugin {
   /** The name of the plugin */
   name = "conventional-commits";
 
+  /** The options of the plugin */
+  private readonly options: ConventionalCommitsOptions;
+
+  /** The getBump function stored on the plugin so we don't load the config for every commit */
+  private storedGetBump?: (
+    message: string
+  ) =>
+    | Promise<SEMVER.major | SEMVER.minor | SEMVER.patch | "skip" | undefined>
+    | undefined;
+
+  /** Initialize the plugin with it's options */
+  constructor(options: ConventionalCommitsOptions = {}) {
+    this.options = options;
+  }
+
   /** Tap into auto plugin points. */
   apply(auto: Auto) {
+    /** Get the bump using conventional commits */
+    const getBump = async (message: string) => {
+      if (!this.storedGetBump) {
+        /** Load the implementation */
+        this.storedGetBump = async (message: string) => {
+          if (!auto.semVerLabels) {
+            return;
+          }
+
+          const conventionalCommit = parse(message);
+          const preset = this.options.preset
+            ? await conventionalChangelogPresetLoader(this.options.preset)
+            : defaultPreset;
+          const config = await presetResolver(preset);
+          const whatBump =
+            config.recommendedBumpOpts?.whatBump ||
+            defaultPreset.recommendedBumpOpts.whatBump;
+          const VERSIONS = [
+            SEMVER.major,
+            SEMVER.minor,
+            SEMVER.patch,
+            "skip",
+          ] as const;
+          const result = whatBump([conventionalCommit]);
+
+          if (result?.level !== null && result?.level !== undefined) {
+            const bump = VERSIONS[result.level];
+            return bump;
+          }
+        };
+      }
+
+      return this.storedGetBump(message);
+    };
+
     auto.hooks.onCreateLogParse.tap(this.name, (logParse) => {
-      logParse.hooks.parseCommit.tap(this.name, (commit) => {
+      logParse.hooks.parseCommit.tapPromise(this.name, async (commit) => {
         if (!auto.semVerLabels) {
           return commit;
         }
 
         try {
-          const [conventionalCommit] = applyPlugins(
-            mappers.increment,
-            parse(commit.subject)
-          );
-          // conventional commits will return a falsy value when no increment is detected (e.g., chore/perf/refactor commits)
-          const commitIncrement =
-            (conventionalCommit.increment as VersionLabel) || "skip";
-          const incrementLabel = auto.semVerLabels.get(commitIncrement);
+          const label = await getBump(`${commit.subject}\n\n${commit.rawBody}`);
+
+          if (!label) {
+            return commit;
+          }
+
+          const incrementLabel = auto.semVerLabels.get(label);
           const allSemVerLabels = [
             auto.semVerLabels.get(SEMVER.major),
             auto.semVerLabels.get(SEMVER.minor),
@@ -37,11 +159,10 @@ export default class ConventionalCommitsPlugin implements IPlugin {
           );
 
           if (
-            conventionalCommit.header &&
             incrementLabel &&
             !commit.labels.some((l) => allSemVerLabels.includes(l))
           ) {
-            commit.labels = [...commit.labels, ...incrementLabel];
+            commit.labels = [...commit.labels, incrementLabel[0]];
           }
         } catch (error) {
           auto.logger.verbose.info(
@@ -63,17 +184,22 @@ export default class ConventionalCommitsPlugin implements IPlugin {
           const prCommits = await auto.git.getCommitsForPR(
             commit.pullRequest.number
           );
+          let shouldOmit = false;
 
           // Omit the commit if one of the commits in the PR contains a CC message since it will already be counted
-          return Boolean(
-            prCommits.find((c) => {
+          await Promise.all(
+            prCommits.map(async (c) => {
               try {
-                return Boolean(parse(c.commit.message)[0].header);
-              } catch (error) {
-                return false;
-              }
+                const label = await getBump(c.commit.message);
+
+                if (label) {
+                  shouldOmit = true;
+                }
+              } catch (error) {}
             })
           );
+
+          return shouldOmit;
         }
       });
     });
