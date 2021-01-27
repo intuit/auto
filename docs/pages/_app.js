f54055f487772d59aef6c23a49417d8b1f19dd5e
Andrew Lisowski <lisowski54@gmail.com>
2021-01-27 09:11:03 -0800

    r

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -7,7 +7,6 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import "next-ignite/dist/main.css";
-import "../css/syntax-highlighting-overrides.css";
 
 // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 config.autoAddCss = false;
