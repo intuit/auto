5de65e6c7e62d1c5443b75ff698f32c67afa1247
Andrew Lisowski <lisowski54@gmail.com>
2021-02-18 11:34:00 -0800

    move old slack plugin logic to microsoft teams plugin

---
+++ plugins/microsoft-teams/__tests__/index.test.ts
@@ -0,0 +1,397 @@
+import Auto from "@auto-it/core";
+import makeCommitFromMsg from "@auto-it/core/src/__tests__/make-commit-from-msg";
+import { dummyLog } from "@auto-it/core/src/utils/logger";
+import { makeHooks } from "@auto-it/core/src/utils/make-hooks";
+import { defaultLabels } from "@auto-it/core/dist/semver";
+import { execSync } from "child_process";
+import createHttpsProxyAgent from "https-proxy-agent";
+
+import MicrosoftTeamsPlugin, { sanitizeMarkdown } from "../src";
+
+const fetchSpy = jest.fn();
+// @ts-ignore
+jest.mock("node-fetch", () => (...args) => {
+  fetchSpy(...args);
+});
+
+beforeEach(() => {
+  fetchSpy.mockClear();
+});
+
+const mockResponse = [
+  {
+    data: {
+      html_url: "https://git.hub/some/project/releases/v1.0.0",
+      name: "v1.0.0",
+    },
+  },
+];
+
+// For the purpose of this test, we use the current branch as the "prerelease" branch to fake being on a "next" branch
+const nextBranch = execSync("git rev-parse --abbrev-ref HEAD", {
+  encoding: "utf8",
+}).trim();
+
+const mockAuto = {
+  git: {},
+  logger: dummyLog(),
+} as any;
+
+describe("createPost", () => {
+  test("doesn't post with no new version", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({ hooks } as Auto);
+
+    await hooks.afterRelease.promise({
+      lastRelease: "0.1.0",
+      commits: [],
+      releaseNotes: "# My Notes",
+    });
+
+    expect(plugin.createPost).not.toHaveBeenCalled();
+  });
+
+  test("doesn't post with no commits", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({ hooks, options: {} } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [],
+      releaseNotes: "# My Notes",
+    });
+
+    expect(plugin.createPost).not.toHaveBeenCalled();
+  });
+
+  test("doesn't post with skip release label", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({
+      hooks,
+      options: {},
+      config: { labels: defaultLabels },
+    } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("skipped", { labels: ["skip-release"] })],
+      releaseNotes: "# My Notes",
+    });
+
+    expect(plugin.createPost).not.toHaveBeenCalled();
+  });
+
+  test("doesn't post without url", async () => {
+    // @ts-ignore
+    const plugin = new MicrosoftTeamsPlugin({ url: undefined });
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({ hooks, options: {} } as Auto);
+
+    await expect(
+      hooks.afterRelease.promise({
+        newVersion: "1.0.0",
+        lastRelease: "0.1.0",
+        commits: [makeCommitFromMsg("a patch")],
+        releaseNotes: "# My Notes",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  test("doesn't post when prerelease branch and using default prereleasePublish setting", async () => {
+    // @ts-ignore
+    const plugin = new MicrosoftTeamsPlugin({
+      url: "https://custom-microsoft-url",
+    });
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({
+      ...mockAuto,
+      hooks,
+      options: {},
+      config: {
+        prereleaseBranches: [nextBranch],
+        labels: defaultLabels,
+      },
+    } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes",
+    });
+    expect(fetchSpy).not.toHaveBeenCalled();
+  });
+
+  test("doesn't post when prerelease branch setting is false", async () => {
+    // @ts-ignore
+    const plugin = new MicrosoftTeamsPlugin({
+      url: "https://custom-microsoft-url",
+      publishPreRelease: false,
+    });
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({
+      ...mockAuto,
+      hooks,
+      options: {},
+      config: {
+        prereleaseBranches: [nextBranch],
+        labels: defaultLabels,
+      },
+    } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes",
+    });
+    expect(fetchSpy).not.toHaveBeenCalled();
+  });
+
+  test("posts when prerelease branch setting is true", async () => {
+    // @ts-ignore
+    const plugin = new MicrosoftTeamsPlugin({
+      url: "https://custom-microsoft-url",
+      publishPreRelease: true,
+    });
+    const hooks = makeHooks();
+
+    jest.spyOn(plugin, "createPost").mockImplementation();
+    // @ts-ignore
+    plugin.apply({
+      ...mockAuto,
+      hooks,
+      options: {},
+      config: { prereleaseBranches: ["next"], labels: defaultLabels },
+    } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes",
+      // @ts-ignore
+      response: mockResponse,
+    });
+    expect(plugin.createPost).toHaveBeenCalledTimes(1);
+  });
+
+  test("should call slack api with minimal config", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown("# My Notes\n- PR [some link](google.com)"),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      undefined
+    );
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].agent).toBeUndefined();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should add more indents to nested lists", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown(
+        "# My Notes\n- PR [some link](google.com)\n - Another note"
+      ),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      undefined
+    );
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].agent).toBeUndefined();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should add more indents to nested lists - 2 spaces", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown(
+        "# My Notes\n- PR [some link](google.com)\n  - Another note"
+      ),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      undefined
+    );
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].agent).toBeUndefined();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should call slack api through http proxy", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+    process.env.http_proxy = "http-proxy";
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown("# My Notes\n- PR [some link](google.com)"),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      createHttpsProxyAgent("mock-url")
+    );
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].agent).not.toBeUndefined();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should remove markdown code types from block", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown(
+        `# My Notes\n\`\`\`json\n{ "foo": "bar" }\`\`\`\n- PR [some link](google.com)`
+      ),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      undefined
+    );
+
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should call slack api through https proxy", async () => {
+    const plugin = new MicrosoftTeamsPlugin("https://custom-microsoft-url");
+    process.env.https_proxy = "https-proxy";
+
+    await plugin.createPost(
+      mockAuto,
+      sanitizeMarkdown("# My Notes\n- PR [some link](google.com)"),
+      "*<https://git.hub/some/project/releases/v1.0.0|v1.0.0>*",
+      createHttpsProxyAgent("mock-url")
+    );
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].agent).not.toBeUndefined();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should call slack api", async () => {
+    const plugin = new MicrosoftTeamsPlugin({ url: "https://custom-microsoft-url" });
+    const hooks = makeHooks();
+    plugin.apply({ hooks, options: {}, ...mockAuto } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes\n- PR [some link](google.com)",
+      // @ts-ignore
+      response: mockResponse,
+    });
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should call slack api with custom atTarget", async () => {
+    const plugin = new MicrosoftTeamsPlugin({
+      url: "https://custom-microsoft-url",
+      atTarget: "here",
+    });
+    const hooks = makeHooks();
+    plugin.apply({ hooks, options: {}, ...mockAuto } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes\n- PR [some link](google.com)",
+      // @ts-ignore
+      response: mockResponse,
+    });
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe(
+      "https://custom-microsoft-url"
+    );
+    expect(fetchSpy.mock.calls[0][1].body.includes("@here")).toBe(true);
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should call slack api in env var", async () => {
+    process.env.MICROSOFT_TEAMS_WEBHOOK_URL = "https://foo.bar";
+    const plugin = new MicrosoftTeamsPlugin();
+    const hooks = makeHooks();
+    plugin.apply({ hooks, options: {}, ...mockAuto } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes\n- PR [some link](google.com)",
+      // @ts-ignore
+      response: mockResponse,
+    });
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][0]).toBe("https://foo.bar");
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+
+  test("should add title", async () => {
+    process.env.MICROSOFT_TEAMS_WEBHOOK_URL = "https://foo.bar";
+    const plugin = new MicrosoftTeamsPlugin({ title: "My Cool Project" });
+    const hooks = makeHooks();
+    plugin.apply({ hooks, options: {}, ...mockAuto } as Auto);
+
+    await hooks.afterRelease.promise({
+      newVersion: "1.0.0",
+      lastRelease: "0.1.0",
+      commits: [makeCommitFromMsg("a patch")],
+      releaseNotes: "# My Notes\n- PR [some link](google.com)",
+      // @ts-ignore
+      response: mockResponse,
+    });
+
+    expect(fetchSpy).toHaveBeenCalled();
+    expect(fetchSpy.mock.calls[0][1].body).toMatchSnapshot();
+  });
+});
--- plugins/microsoft-teams/src/index.ts
+++ plugins/microsoft-teams/src/index.ts
@@ -1,24 +1,76 @@
-import { HttpsProxyAgent } from "https-proxy-agent";
+import { githubToSlack } from "@atomist/slack-messages";
+import createHttpsProxyAgent, { HttpsProxyAgent } from "https-proxy-agent";
 
-import { Auto, InteractiveInit } from "@auto-it/core";
-import SlackPlugin, { ISlackPluginOptions } from "@auto-it/slack";
+import {
+  Auto,
+  InteractiveInit,
+  validatePluginConfiguration,
+  getCurrentBranch,
+} from "@auto-it/core";
 import fetch from "node-fetch";
+import * as t from "io-ts";
+
+const MARKDOWN_LANGUAGE = /^(```)(\S+)$/m;
+
+/** Transform markdown into slack friendly text */
+export const sanitizeMarkdown = (markdown: string) =>
+  githubToSlack(markdown)
+    .split("\n")
+    .map((line) => {
+      // Strip out the ### prefix and replace it with *<word>* to make it bold
+      if (line.startsWith("#")) {
+        return `*${line.replace(/^[#]+/, "")}*`;
+      }
+
+      // Give extra padding to nested lists
+      if (line.match(/^\s+•/)) {
+        return line.replace(/^\s+•/, "   •");
+      }
+
+      // Strip markdown code block type. Slack does not render them correctly.
+      if (line.match(MARKDOWN_LANGUAGE)) {
+        return line.replace(MARKDOWN_LANGUAGE, "`$2`:\n\n$1");
+      }
+
+      return line;
+    })
+    .join("\n");
+
+const pluginOptions = t.partial({
+  /** URL of the mircosoft teams to post to */
+  url: t.string,
+  /** Who to bother when posting to the channel */
+  atTarget: t.string,
+  /** Allow users to opt into having prereleases posted to mircosoft teams */
+  publishPreRelease: t.boolean,
+  /** Additional Title to add at the start of the mircosoft teams message */
+  title: t.string,
+});
+
+export type IMicrosoftTeamsPluginOptions = t.TypeOf<typeof pluginOptions>;
 
 /** Post your release notes to Slack during `auto release` */
-export default class MicrosoftTeamsPlugin extends SlackPlugin {
+export default class MicrosoftTeamsPlugin {
   /** The name of the plugin */
   name = "microsoft-teams";
 
+  /** The options of the plugin */
+  readonly options: IMicrosoftTeamsPluginOptions;
+
   /** Initialize the plugin with it's options */
-  constructor(options: ISlackPluginOptions | string = {}) {
-    super(
-      typeof options === "string"
-        ? options
-        : {
-            ...options,
-            url: process.env.MICROSOFT_TEAMS_WEBHOOK_URL || options.url || "",
-          }
-    );
+  constructor(options: IMicrosoftTeamsPluginOptions | string = {}) {
+    if (typeof options === "string") {
+      this.options = { url: options, atTarget: "channel" };
+    } else {
+      this.options = {
+        ...options,
+        url: process.env.MICROSOFT_TEAMS_WEBHOOK_URL || options.url || "",
+        atTarget: options.atTarget ? options.atTarget : "channel",
+        publishPreRelease: options.publishPreRelease
+          ? options.publishPreRelease
+          : false,
+      };
+    }
   }
 
   /** Custom initialization for this plugin */
@@ -27,9 +79,82 @@ export default class MicrosoftTeamsPlugin extends SlackPlugin {
       ...vars,
       {
         variable: "MICROSOFT_TEAMS_WEBHOOK_URL",
-        message: "What is the root url of your slack hook? ()",
+        message: "What is the root url of your microsoft teams hook? ()",
       },
     ]);
+  }
+
+  /** Tap into auto plugin points. */
+  apply(auto: Auto) {
+    auto.hooks.validateConfig.tapPromise(this.name, async (name, options) => {
+      // If it's a string thats valid config
+      if (name === this.name && typeof options !== "string") {
+        return validatePluginConfiguration(this.name, pluginOptions, options);
+      }
+    });
+
+    auto.hooks.afterRelease.tapPromise(
+      this.name,
+      async ({ newVersion, commits, releaseNotes, response }) => {
+        // Avoid publishing on prerelease branches by default, but allow folks to opt in if they care to
+        const currentBranch = getCurrentBranch();
+        if (
+          currentBranch &&
+          auto.config?.prereleaseBranches?.includes(currentBranch) &&
+          !this.options.publishPreRelease
+        ) {
+          return;
+        }
+
+        if (!newVersion) {
+          return;
+        }
+
+        const head = commits[0];
+
+        if (!head) {
+          return;
+        }
+
+        const skipReleaseLabels = (
+          auto.config?.labels.filter((l) => l.releaseType === "skip") || []
+        ).map((l) => l.name);
+        const isSkipped = head.labels.find((label) =>
+          skipReleaseLabels.includes(label)
+        );
+
+        if (isSkipped) {
+          return;
+        }
+
+        if (!this.options.url) {
+          throw new Error(
+            `${this.name} url must be set to post a message to ${this.name}.`
+          );
+        }
+
+        const releases =
+          (Array.isArray(response) && response) ||
+          (response && [response]) ||
+          [];
+        const urls = releases.map(
+          (release) =>
+            `*<${release.data.html_url}|${
+              release.data.name || release.data.tag_name
+            }>*`
+        );
+        const releaseUrl = urls.length ? urls.join(", ") : newVersion;
+        const proxyUrl = process.env.https_proxy || process.env.http_proxy;
+        const agent = proxyUrl ? createHttpsProxyAgent(proxyUrl) : undefined;
+
+        await this.createPost(
+          auto,
+          sanitizeMarkdown(releaseNotes),
+          releaseUrl,
+          agent
+        );
+      }
+    );
   }
 
   /** Post the release notes to slack */
